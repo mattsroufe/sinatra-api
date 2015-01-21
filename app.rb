a045b93d732350a1235a6120759c3ad99b864a60
Matt Sroufe <mattsroufe@gmail.com>
2015-01-21 21:03:51 +1300

    adds routes and views

--- app.rb
+++ app.rb
@@ -2,6 +2,42 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
 class Post < ActiveRecord::Base
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
+end
+
+get "/posts/create" do
+ @title = "Create post"
+ @post = Post.new
+ erb :"posts/create"
+end
+
+post "/posts" do
+ @post = Post.new(params[:post])
+ if @post.save
+   redirect "posts/#{@post.id}"
+ else
+   erb :"posts/create"
+ end
+end
+
+get "/posts/:id" do
+ @post = Post.find(params[:id])
+ @title = @post.title
+ erb :"posts/view"
 end
