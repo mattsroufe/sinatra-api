39916d1fc6d7c7cebcc31b448e068d6f833d98aa
Matt Sroufe <mattsroufe@gmail.com>
2015-01-25 21:42:30 +1300

    adds accounts

--- app.rb
+++ app.rb
@@ -7,6 +7,31 @@ class Customer < ActiveRecord::Base
   self.table_name = 'customer'
 
   validates_presence_of :fed_id
+
+  has_many :accounts, :foreign_key => :cust_id
+end
+
+class Account < ActiveRecord::Base
+  self.table_name = "account"
+
+  belongs_to :customer, :foreign_key => :cust_id
+end
+
+class Accounts < Grape::API
+  resource :accounts do
+    desc "Returns all accounts."
+    get '/' do
+      Account.all
+    end
+
+    desc "Returns a single account record."
+    params do
+      requires :id, type: Integer, desc: "Account id."
+    end
+    get '/:id' do
+      Account.find(params[:id])
+    end
+  end
 end
 
 module Bank
@@ -14,6 +39,8 @@ module Bank
     version 'v1'
     format :json
     prefix 'api'
+
+    mount Accounts
 
     resource :customers do
       desc 'Returns all customers.'
@@ -35,6 +62,14 @@ module Bank
       end
       post do
         Customer.create!(fed_id: params[:fed_id])
+      end
+
+      desc "Returns a single customer's accounts"
+      params do
+        requires :id, type: Integer, desc: "Customer id."
+      end
+      get '/:id/accounts' do
+        Customer.find(params[:id]).accounts
       end
     end
 
