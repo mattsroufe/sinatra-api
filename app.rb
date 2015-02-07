5071f87700386990f9a4efdb0cc3e995b1b3b621
Matt Sroufe <mattsroufe@gmail.com>
2015-02-07 13:02:30 +1300

    moves customer endpoints into Customers

--- app.rb
+++ app.rb
@@ -34,6 +34,40 @@ class Accounts < Grape::API
   end
 end
 
+class Customers < Grape::API
+
+  resource :customers do
+    desc 'Returns all customers.'
+    get '/' do
+      Customer.all
+    end
+
+    desc "Returns a single customer."
+    params do
+      requires :id, type: Integer, desc: "Customer id."
+    end
+    get '/:id' do
+      Customer.find(params[:id])
+    end
+
+    desc "Create a customer."
+    params do
+      requires :fed_id, type: String, desc: "Customer federal id number."
+    end
+    post do
+      Customer.create!(fed_id: params[:fed_id])
+    end
+
+    desc "Returns a single customer's accounts"
+    params do
+      requires :id, type: Integer, desc: "Customer id."
+    end
+    get '/:id/accounts' do
+      Customer.find(params[:id]).accounts
+    end
+  end
+end
+
 module Bank
   class API < Grape::API
     version 'v1'
@@ -41,37 +75,7 @@ module Bank
     prefix 'api'
 
     mount Accounts
-
-    resource :customers do
-      desc 'Returns all customers.'
-      get '/' do
-        Customer.all
-      end
-
-      desc "Returns a single customer."
-      params do
-        requires :id, type: Integer, desc: "Customer id."
-      end
-      get '/:id' do
-        Customer.find(params[:id])
-      end
-
-      desc "Create a customer."
-      params do
-        requires :fed_id, type: String, desc: "Customer federal id number."
-      end
-      post do
-        Customer.create!(fed_id: params[:fed_id])
-      end
-
-      desc "Returns a single customer's accounts"
-      params do
-        requires :id, type: Integer, desc: "Customer id."
-      end
-      get '/:id/accounts' do
-        Customer.find(params[:id]).accounts
-      end
-    end
+    mount Customers
 
     add_swagger_documentation api_version: 'v1'
   end
