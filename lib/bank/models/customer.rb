ac5aacc96c78ac2bd3412ec4e756790bda9f7987
Matt Sroufe <mattsroufe@gmail.com>
2015-04-03 21:50:57 +1300

    adds cust_type_cd validation on customer

--- lib/bank/models/customer.rb
+++ lib/bank/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ActiveRecord::Base
   self.table_name = 'customer'
 
   validates_presence_of :fed_id, :message => "id can't be blank"
+  validates_presence_of :cust_type_cd
 
   has_many :accounts, :foreign_key => :cust_id
 end
