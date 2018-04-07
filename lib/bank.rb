0078545542316120ab03bc644d01f2f2e53562b6
Matt Sroufe <mattsroufe@Matts-MacBook-Pro.local>
2018-04-07 17:10:47 +1200

    require files dynamically

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,16 +1,3 @@
-require_relative './bank/models/account'
-require_relative './bank/models/branch'
-require_relative './bank/models/business'
-require_relative './bank/models/customer'
-require_relative './bank/models/individual'
-require_relative './bank/models/product'
-require_relative './bank/models/product_type'
-require_relative './bank/models/employee'
-require_relative './bank/models/department'
-require_relative './bank/models/user'
-
-require_relative './bank/queries/employees_query'
-
 require 'fast_jsonapi'
 
-require_relative './bank/serializers/employee_serializer'
+Dir[File.expand_path "lib/**/*.rb"].each{|f| require_relative(f)}
