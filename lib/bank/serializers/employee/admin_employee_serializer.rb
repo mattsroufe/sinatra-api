fa659b44ff7ef1ad6e7e055ab80b7f6b78f2dac6
Matt Sroufe <mattsroufe@gmail.com>
2018-04-15 07:50:20 +1200

    add admin test

--- lib/bank/serializers/employee/admin_employee_serializer.rb
+++ lib/bank/serializers/employee/admin_employee_serializer.rb
@@ -2,5 +2,5 @@ class AdminEmployeeSerializer
   include FastJsonapi::ObjectSerializer
   set_type :employee
 
-  attributes :birth_date, :emp_no, :first_name, :gender, :hire_date, :last_name
+  attributes :birth_date, :first_name, :gender, :hire_date, :last_name
 end
